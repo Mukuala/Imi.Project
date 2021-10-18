174734dc7d03c9bacbb29ed6b6c2269ff62c9de7
Mpofuri Mukuala Deo gracias <55794153+Mukuala@users.noreply.github.com>
2021-10-18 12:06:31 +0200

    Add migration/DBcontext and udate startup/appsettings

---
+++ src/Imi.Project.Api.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Imi.Project.Api.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<MovieGenre> MovieGenres { get; set; }
+        public DbSet<MovieActor> MovieActors { get; set; }
+        public DbSet<Favorite> Favorites { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<MovieGenre>()
+                .HasOne<Genre>(mg => mg.Genre)
+                .WithMany(g => g.Movies)
+                .HasForeignKey(mg => mg.GenreId);
+            modelBuilder.Entity<MovieActor>()
+                .HasOne<Actor>(ma => ma.Actor)
+                .WithMany(a => a.Movies)
+                .HasForeignKey(ma => ma.ActorId);
+            modelBuilder.Entity<Favorite>()
+                .HasOne<ApplicationUser>(f => f.ApplicationUser)
+                .WithMany(a => a.FavoriteMovies)
+                .HasForeignKey(f => f.ApplicationUserId);
+            modelBuilder.Entity<Watchlist>()
+                .HasOne<ApplicationUser>(f => f.ApplicationUser)
+                .WithMany(a => a.WatchlistMovies)
+                .HasForeignKey(f => f.ApplicationUserId);
+
+        }
+
+    }
+}
--- src/Imi.Project.Api/Startup.cs
+++ src/Imi.Project.Api/Startup.cs
@@ -1,7 +1,9 @@
+using Imi.Project.Api.Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,11 @@ namespace Imi.Project.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConnectionStrings:DatabaseConnection"]);
+                options.EnableSensitiveDataLogging();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
