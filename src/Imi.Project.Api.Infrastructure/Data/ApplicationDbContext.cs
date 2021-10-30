f7827428735967115af040c0ebf367a8f13c72a3
Mpofuri Mukuala Deo gracias <55794153+Mukuala@users.noreply.github.com>
2021-10-30 16:58:44 +0200

    Modify DbContext

--- src/Imi.Project.Api.Infrastructure/Data/ApplicationDbContext.cs
+++ src/Imi.Project.Api.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Imi.Project.Api.Core.Entities;
+using Imi.Project.Api.Infrastructure.Data.Seeding;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -7,7 +8,7 @@ using System.Text;
 
 namespace Imi.Project.Api.Infrastructure.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ApplicationDbContext : DbContext
     {
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Actor> Actors { get; set; }
@@ -24,22 +25,51 @@ namespace Imi.Project.Api.Infrastructure.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<MovieGenre>()
                 .HasOne<Genre>(mg => mg.Genre)
                 .WithMany(g => g.Movies)
                 .HasForeignKey(mg => mg.GenreId);
+            modelBuilder.Entity<MovieGenre>()
+                .HasOne<Movie>(mg => mg.Movie)
+                .WithMany(m => m.Genres)
+                .HasForeignKey(mg => mg.MovieId);
+
             modelBuilder.Entity<MovieActor>()
                 .HasOne<Actor>(ma => ma.Actor)
                 .WithMany(a => a.Movies)
                 .HasForeignKey(ma => ma.ActorId);
+            modelBuilder.Entity<MovieActor>()
+                .HasOne<Movie>(ma => ma.Movie)
+                .WithMany(m => m.Actors)
+                .HasForeignKey(ma => ma.MovieId);
+
             modelBuilder.Entity<Favorite>()
                 .HasOne<ApplicationUser>(f => f.ApplicationUser)
                 .WithMany(a => a.FavoriteMovies)
                 .HasForeignKey(f => f.ApplicationUserId);
+            modelBuilder.Entity<Favorite>()
+                .HasOne<Movie>(f => f.Movie)
+                .WithMany(m => m.UsersFavorite)
+                .HasForeignKey(f => f.MovieId);
+
             modelBuilder.Entity<Watchlist>()
-                .HasOne<ApplicationUser>(f => f.ApplicationUser)
+                .HasOne<ApplicationUser>(w => w.ApplicationUser)
                 .WithMany(a => a.WatchlistMovies)
-                .HasForeignKey(f => f.ApplicationUserId);
+                .HasForeignKey(w => w.ApplicationUserId);
+            modelBuilder.Entity<Watchlist>()
+                .HasOne<Movie>(w => w.Movie)
+                .WithMany(m => m.UsersWatchlist)
+                .HasForeignKey(w => w.MovieId);
+
+            UserSeeder.Seed(modelBuilder);
+            GenreSeeder.Seed(modelBuilder);
+            ActorSeeder.Seed(modelBuilder);
+            MovieSeeder.Seed(modelBuilder);
+            FavoriteSeeder.Seed(modelBuilder);
+            WatchlistSeeder.Seed(modelBuilder);
+            MovieActorSeeder.Seed(modelBuilder);
+            MovieGenreSeeder.Seed(modelBuilder);
 
         }
 
